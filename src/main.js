466d60118c2577362002c8dd2d785f6b0c690f4b
JackHuo <cji4dk40@gmail.com>
2021-09-29 19:15:52 +0800

    feat: dark mode feat: popup widow feat: checkbox focus effect

---
+++ src/main.js
@@ -0,0 +1,91 @@
+import './scss/main.scss'
+
+console.log('JS Loaded!!')
+
+const tbody = document.querySelector('.tbody')
+tbody.innerHTML = ''
+
+for(let i = 1 ; i < 20 ; i++){
+  tbody.innerHTML += `<tr class="table__row table__body__row" id="row-${i}">
+              <td class="table__cell table__cell--checkbox">
+              <input type="checkbox" name="body_checkbox" id="body_checkbox" data-id="row-${i}">
+              </td>
+              <td class="table__cell table__cell--id">ID</td>
+              <td class="table__cell table__cell--name">NAME</td>
+              <td class="table__cell table__cell--ad">
+                <div class="advertiser">Advertiser</div>
+                <span class="gruop">Gruop</span>
+              </td>
+              <td class="table__cell table__cell--description">Description</td>
+              <td class="table__cell table__cell--price">price</td>
+              <td class="table__cell table__cell--startime">Start Time</td>
+              <td class="table__cell table__cell--endtime">End Time</td>
+              <td class="table__cell table__cell--action">
+              <img class="cell__action__icon" src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/menu.svg" id="action__input_1" alt="menu" />
+              <div class="action__menu hidden" role="dialog" aria-modal="true" aria-labelledby="action__input_1" id="action__menu_1">
+                <menu class="menu__body">
+                  <menuitem class="menu__item">
+                  <img src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/duplicate.svg" class="menu__item__icon" />
+                  <span>Duplicate</span>
+                  </menuitem>
+                  <menuitem class="menu__item">
+                  <img src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/edit.svg" class="menu__item__icon" />
+                  <span>Edit</span>
+                  </menuitem>
+                  <menuitem class="menu__item">
+                  <img src="https://raw.githubusercontent.com/WendellLiu/alphacamp-frontend-course/master/data-table/static/icons/delete.svg" class="menu__item__icon" />
+                  <span>Delete</span>
+                  </menuitem>
+                </menu>
+              </div>
+            </td>
+          </tr>`
+}
+
+// target the input element
+const bodyRows = document.querySelectorAll(".table__body__row");
+const darkMode = document.querySelector('#dark__mode__toggle')
+const headerCheckBox = document.querySelector('#header_checkbox')
+
+// click handler
+const bodyRowClicked = event => {
+  const target = event.target
+  if(target.matches('.cell__action__icon')){
+    // target the menu element
+    const menu = target.nextElementSibling
+    menu.classList.toggle("hidden");
+  } else if (target.id === 'body_checkbox'){
+    const row = document.getElementById(target.dataset.id)
+    if (target.checked){
+      row.classList.add("focus")
+    }else{
+      row.classList.remove("focus")
+    }
+  }
+};
+
+const darkModeToggleHandler = e => {
+  if(e.target.checked){
+    document.documentElement.setAttribute('data-theme' , 'dark')
+  }else{
+    document.documentElement.setAttribute('data-theme' , 'light')
+  }
+}
+
+const headerCheckBoxClicked = e =>{
+  const bodyCheckBox = document.querySelectorAll('#body_checkbox')
+  if(e.target.checked){
+    bodyCheckBox.forEach(checkBox => {
+      checkBox.checked = true
+      bodyRows.forEach(row => row.classList.add("focus"))
+     
+    })
+  }else{
+    bodyCheckBox.forEach(checkBox => checkBox.checked = false)
+    bodyRows.forEach(row => row.classList.remove("focus"))
+  }
+}
+// bind the event
+bodyRows.forEach(bodyRow => bodyRow.addEventListener("click", bodyRowClicked))
+darkMode.addEventListener('change', darkModeToggleHandler)
+headerCheckBox.addEventListener('change', headerCheckBoxClicked)
